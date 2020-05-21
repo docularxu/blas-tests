095a9a4bceb31bebd134a06252ac9b80309add83
Guodong Xu <guodong.xu@linaro.com>
2020-05-21 07:05:43 +0000

    call_test.sh: improved call_test with start/ stop/ parameters
    Signed-off-by: Guodong Xu <guodong.xu@linaro.com>

--- call_test.sh
+++ call_test.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+k_start=1
+k_end=480
+
 for x in $(ls test*)
 do
 echo "Tests $x"
+
 export OPENBLAS_INCX=1
 export OPENBLAS_INCY=1
 echo "INCX=${OPENBLAS_INCX} INCY=${OPENBLAS_INCY}"
-./$x 5  177 
+./$x $k_start $k_end
+
 export OPENBLAS_INCY=4
 echo "INCX=${OPENBLAS_INCX} INCY=${OPENBLAS_INCY}"
+./$x $k_start $k_end
 
-./$x 5 177
 export OPENBLAS_INCX=4
 echo "INCX=${OPENBLAS_INCX} INCY=${OPENBLAS_INCY}"
+./$x $k_start $k_end
 
-./$x 5 177
 export OPENBLAS_INCX=7
 export OPENBLAS_INCY=7
 echo "INCX=${OPENBLAS_INCX} INCY=${OPENBLAS_INCY}"
+./$x $k_start $k_end
 
-./$x 5 177
 done
